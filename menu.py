885c5af56cc498d7ec4b157db85c7ad48a1748d6
Pratik Mukherjee <49201247+pratik-276@users.noreply.github.com>
2020-03-24 07:55:54 +0530

    main function updated

--- menu.py
+++ menu.py
@@ -61,30 +61,6 @@ def net_connect():
 		
 	f.close()
 	os.system("rm -f net.txt")
-
-
-
-
-def rhel_essentials():
-	#write your code here
-	print("rhel_essentials")
-
-def file_manager():
-	#write your code here
-	print("file and package manager")
-
-def network():
-	#write your code here
-	print("network and security")
-
-def docker():
-	#write your code here
-	print("docker services")
-		
-
-def other():
-	#write your code here
-	print("other services")
 		
 			
 #code for test run
@@ -92,7 +68,23 @@ if net_connect():
 	print("internet working")
 else:
 	print("internet not working")	
-		
 	
-		
+default()
+print('''\tWHAT DO YOU WANT TO DO?
+\t1. Set up Web server''')
 
+take = int(input("\nEnter your choice: "))
+	
+if take is 1:
+	if net_connect():
+		if mount_check():
+			yum_config()
+			yum_install('httpd')
+			print('Enabling web server will require root password. Enter below.\n')
+			os.system('su -c "systemctl enabling httpd"')
+			file_add()
+			print("Web server configured successfully at port 80\n")
+			take1 = input('Do you want to check your website?(y/n)')
+			if take1 is 'y':
+				personalised_link()
+			
